a6da9219303545765d719c573049ebc9244f83c7
Cedric <yacmov@gmail.com>
2024-03-26 10:46:43 +0800

    feat: S9C2 completed

--- session-09/assets/js/scripts.js
+++ session-09/assets/js/scripts.js
@@ -1,1 +1,11 @@
 // Put your challenge code here
+
+// Create two variables
+let length = 5;
+let width = 8;
+
+// Calculate the area of rectangle
+let area = length * width
+
+// Display on the console
+console.log("This is the size of rectangle: " + Math.round(area))
