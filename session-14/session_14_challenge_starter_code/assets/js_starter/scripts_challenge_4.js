9e582060d8a70953a8769d1515f5f9cd0967327e
Cedric Ko <yacmov@gmail.com>
2024-05-07 10:33:05 +0800

    session_14 challendges

--- session-14/session_14_challenge_starter_code/assets/js_starter/scripts_challenge_4.js
+++ session-14/session_14_challenge_starter_code/assets/js_starter/scripts_challenge_4.js
@@ -1,6 +1,6 @@
 // Challenge 4 Starter
 // Get the form element
-
+let form = document.querySelector("#save-me")
 
 // localStorage prefix
 let prefix = 'autosave_';
@@ -10,24 +10,40 @@ let prefix = 'autosave_';
  * @param  {Event} event The event object
  */
 function inputHandler (event) {
-    //
+    let field = event.target;
+
+    if (!field.id) return;
+
+    localStorage.setItem(prefix + field.id, field.value);
 }
 
 /**
  * Clear all of the saved fields from storage
  */
 function clearStorage () {
-    //
+    let fields = form.elements;
+
+    for (let field of fields){
+        localStorage.removeItem(prefix + field.id);
+    }
 }
 
 /**
  * Load saved data from localStorage
  */
 function loadSaved () {
-    //
+    let fields = form.elements;
+
+    for (let field of fields) {
+        let saved = localStorage.getItem(prefix + field.id)
+        if (!saved) continue;
+        field.value = saved;
+    }
 }
 
 // Load saved data from localStorage
-
+loadSaved();
 
 // Listen for DOM events
+form.addEventListener("input", inputHandler)
+form.addEventListener("submit", clearStorage)
